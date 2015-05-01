d2e588a14dc82b1105c3d232b8f536215fe8bf9f
Flavius Aspra <flavius.as@gmail.com>
2015-05-01 19:47:12 +0200

    less verbosity in pacman wrapper function

--- .zsh/autoload/post/50_archlinux_management.zsh
+++ .zsh/autoload/post/50_archlinux_management.zsh
@@ -1,7 +1,8 @@
 # Install a package and commit this to the repository in /etc.
 # Parameters: the package to be installed.
 function installpkg() {
-    pacman --noconfirm -S "$1"
+    echo "INSTALL: '$1'"
+    pacman --noconfirm -S "$1" > /dev/null
     commit_all "/etc" "[INSTALL] $1"
     commit_all "/var/log" "[INSTALL] $1"
     #TODO: write data to a syslog-ng file, which will be replayed back later
