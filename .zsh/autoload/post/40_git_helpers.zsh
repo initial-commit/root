0b1e977db98a14595653b84b1f7801e39cf2297a
Flavius Aspra <flavius.as@gmail.com>
2015-05-01 15:56:53 +0200

    git helpers zsh functions

---
+++ .zsh/autoload/post/40_git_helpers.zsh
@@ -0,0 +1,27 @@
+# Initialise a git repository in a given directory.
+# Parameters: the directory where to init the repo.
+function start_versioning() {
+    pushd "$1" > /dev/null
+    if [[ !( -d './.git') ]]; then
+        git init
+        git add . > /dev/null
+        git commit -m "Initial commit"
+    fi
+    #TODO: write data to a syslog-ng file, which will be replayed back later
+    popd > /dev/null
+}
+
+# Commit all changes in the current directory.
+# If there is no git repository initialised at destination or there is nothing
+# unstaged, exit silently.
+# Parameters: - the directory where to make the commit
+#             - the commit message
+function commit_all() {
+    pushd "$1" > /dev/null
+    if [[ -d './.git' && -z $(git status --porcelain) ]]; then
+        git add .
+        git commit -m "$2"
+    fi
+    popd > /dev/null
+}
+
