de577ebb7a7f1eab51a18b5bc9ce70b311e3b555
Flavius Aspra <flavius.as@gmail.com>
2015-05-01 19:45:08 +0200

    less verbosity in git wrapper functions

--- .zsh/autoload/post/40_git_helpers.zsh
+++ .zsh/autoload/post/40_git_helpers.zsh
@@ -5,7 +5,7 @@ function start_versioning() {
     if [[ !( -d './.git') ]]; then
         git init
         git add . > /dev/null
-        git commit -m "Initial commit"
+        git commit -m "Initial commit" > /dev/null
     fi
     #TODO: write data to a syslog-ng file, which will be replayed back later
     popd > /dev/null
@@ -19,8 +19,8 @@ function start_versioning() {
 function commit_all() {
     pushd "$1" > /dev/null
     if [[ -d './.git' && ! -z $(git status --porcelain) ]]; then
-        git add -A .
-        git commit -m "$2"
+        git add -A . > /dev/null
+        git commit -m "$2" > /dev/null
     fi
     popd > /dev/null
 }
